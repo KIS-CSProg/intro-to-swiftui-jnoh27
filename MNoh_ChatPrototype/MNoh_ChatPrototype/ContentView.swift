676ee1656f79c0a8a8e0a2b9010d761397dcab8a
jnoh27 <jnoh27@student.kis.or.kr>
2026-04-23 13:03:52 +0900

    Create ChatPrototype Project

---
+++ MNoh_ChatPrototype/MNoh_ChatPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MNoh_ChatPrototype
+//
+//  Created by Jeongmin (Michael) Noh on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MNoh_ChatPrototype/MNoh_ChatPrototype/MNoh_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MNoh_ChatPrototypeApp.swift
+//  MNoh_ChatPrototype
+//
+//  Created by Jeongmin (Michael) Noh on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct MNoh_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
